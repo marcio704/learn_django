e1d29bfa0520da3261343b07239469444cf4199b
Marcio Santiago <marcio.santiago@guiato.com.br>
2015-05-20 18:58:32 -0300

    Added template schema for html; Added detail and all posts pages

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^posts', views.posts, name='posts'),
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -5,26 +5,39 @@ from django.http import HttpResponse
 from .models import Post
 from .models import Category
 from django.views import generic
+import math
 
 
 # Create your views here.
 
 
 """
-def index(request):
-	speech_list = Post.objects.all()
-	context = {'speech_list': speech_list}
-	return render(request, 'index.html', context)
-"""
 class IndexView(generic.ListView):
     template_name = 'blog/index.html'
     context_object_name = 'post_list'
 
     def get_queryset(self):
-        """Return last 5 posts."""
+        # Return last 5 posts.
         return Post.objects.all()[:5]
+"""
+def index(request):
+    post_list = Post.objects.all()[:5]
+    category_list = Category.objects.all()
+    half_size_ceil = int(math.ceil(len(category_list)/2))  
+    half_size_floor = int(math.floor(len(category_list)/2))
+    category_list_1 = category_list [:half_size_ceil]
+    category_list_2 = category_list [len(category_list) - half_size_floor:]
+    
+    context = {'post_list': post_list, 'category_list_1': category_list_1, 'category_list_2': category_list_2}
+    return render(request, 'blog/index.html', context)
 
 
 class DetailView(generic.DetailView):
     model = Post
     template_name = 'blog/detail.html'        
+
+def posts(request):
+    post_list = Post.objects.all()
+    context = {'post_list': post_list}
+
+    return render(request, 'blog/all_posts.html', context)
