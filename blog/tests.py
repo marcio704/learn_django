2ae077773f856a267eb19254f52a1d853c1cfb55
Marcio Santiago <marcio.santiago@guiato.com.br>
2015-06-10 13:20:42 -0300

    Added test example

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 
 # Create your tests here.
+
+### Tests for Views
+class BlogViewTests(TestCase):
+	def test_if_index_view_is_loaded(self):
+		response = self.client.get(reverse('blog:index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertGreater(len(response.content), 0)
