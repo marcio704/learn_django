1ca633bd41090df1b535dc6f6adbb0ae49e389e9
Marcio Santiago <marcio.santiago@guiato.com.br>
2015-06-29 16:28:13 -0300

    Added consumers to deploy file

--- deploy_prd.sh
+++ deploy_prd.sh
@@ -4,5 +4,8 @@ killall -9 uwsgi
 #Set env variable to prd settings
 export DJANGO_SETTINGS_MODULE=learn_django.settings.prd
 
+#Start threads for rabbitMQ consumers
+python start_consumers.py &
+
 #Start server
 uwsgi --ini deploy.ini:prd
