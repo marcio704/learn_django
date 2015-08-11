34e56a5c769956ee481db2e6aeb64589bc99396f
Marcio Santiago <marcio.santiago@guiato.com.br>
2015-08-11 13:27:51 -0300

    Improved prd deploy script

--- deploy_prd.sh
+++ deploy_prd.sh
@@ -4,10 +4,14 @@ killall -9 uwsgi
 #Set env variable to prd settings
 export DJANGO_SETTINGS_MODULE=learn_django.settings.prd
 
-#Start threads for rabbitMQ consumers
+
+#Kill current celery instance
 killall -9 celery
+#Start threads for rabbitMQ consumers
 nohup celery -A blog worker -l info > /tmp/celery.log 2>&1 &
 
+#Kill current es instance
+killall -9 java
 #Start elasticsearch nodes
 nohup /etc/elasticsearch-1.6.0/bin/elasticsearch > /tmp/celery.log 2>&1 &
 
